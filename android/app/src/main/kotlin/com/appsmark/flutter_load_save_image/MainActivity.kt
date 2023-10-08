6ad6aa2eee4e0739ce780efbf1433bc13ac8680b
Martin Keesen <75047596+appsmark@users.noreply.github.com>
2023-10-08 22:36:20 +0200

    iOS Image from Gallery

---
+++ android/app/src/main/kotlin/com/appsmark/flutter_load_save_image/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.appsmark.flutter_load_save_image
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+
+Future<void> main() async {
+  // Ensure that plugin services are initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Device orientation
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  // Suppress statusbar
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      overlays: [SystemUiOverlay.bottom]);
+
+  //Permissions.requestAll();
+
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFF731816),
+      ),
+      home: const Experiment(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
+
+class Experiment extends StatefulWidget {
+  const Experiment({super.key});
+
+  @override
+  State<Experiment> createState() => _Experiment();
+}
+
+class _Experiment extends State<Experiment> {
+  File? image;
+  final GlobalKey _globalKey = GlobalKey();
+
+  Future loadFromGallery() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (image == null) return;
+
+      final imageTemp = File(image.path);
+      setState(() {
+        this.image = imageTemp;
+      });
+    } on PlatformException catch (e) {
+      debugPrint('Failed to pick image: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color(0xFF731816),
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF731816),
+          foregroundColor: const Color(0xFF00AADE),
+          title: const Text("SAVE & SHOW LOCAL IMAGES"),
+        ),
+        body: Center(
+          child: Column(children: [
+            MaterialButton(
+                child: Image.asset(
+                  "assets/images/gallery.png",
+                  height: 0.1 * MediaQuery.of(context).size.height,
+                ),
+                onPressed: () {
+                  loadFromGallery();
+                }),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(width: 3, color: const Color(0xFF00AADE)),
+              ),
+              child: RepaintBoundary(
+                key: _globalKey,
+                child: SizedBox(
+                  height: 0.5 * MediaQuery.of(context).size.height,
+                  width: 0.3 * MediaQuery.of(context).size.width,
+                  child: image != null
+                      ? Image.file(image!)
+                      : Center(
+                          child: Text("PLACEHOLDER",
+                              style: TextStyle(
+                                  color: const Color(0xFFF9B234),
+                                  fontSize: 0.03 *
+                                      MediaQuery.of(context).size.height)),
+                        ),
+                ),
+              ),
+            ),
+          ]),
+        ));
+  }
+}
